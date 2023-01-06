548085913b1b20422cb51f2daf34eab0ad9dc8c1
MakerSuite <133332883+MakerSuite@users.noreply.github.com>
2023-01-06 10:13:28 +0000

    Changed license to MIT.

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEv
+import java.awt.event.KeyEvent;
+
+import java
