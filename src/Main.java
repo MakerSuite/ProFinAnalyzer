25f90e5f8fbad32344ce1528f3b06b32dab17b86
MakerSuite <133332883+MakerSuite@users.noreply.github.com>
2023-02-24 20:21:19 +0000

    Translated using Weblate (Urdu)

--- src/Main.java
+++ src/Main.java
@@ -34,4 +34,4 @@ public class Main {
 		menu = new JMenu("A Menu");
 		menu.setMnemonic(KeyEvent.VK_A);
 		menu.getAccessibleContext().setAccessibleDescription(
-		        "The only 
+		        "The only menu in this program
