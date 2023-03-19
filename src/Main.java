f27b63e0d8d2937e18706f800f62d347ae3eb569
MakerSuite <133332883+MakerSuite@users.noreply.github.com>
2023-03-19 22:13:31 +0000

    gruntfile

--- src/Main.java
+++ src/Main.java
@@ -46,4 +46,5 @@ public class Main {
 		        "This doesn't really do anything");
 		menu.add(menuItem);
 
-		menuItem = new JMenuItem("Both text and ico
+		menuItem = new JMenuItem("Both text and icon",
+		             
