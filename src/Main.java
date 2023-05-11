24a6ed8818138f982bfec3dda6f931caac2b4f3c
MakerSuite <133332883+MakerSuite@users.noreply.github.com>
2023-05-11 18:48:17 +0000

    Update ISSUE_TEMPLATE

--- src/Main.java
+++ src/Main.java
@@ -85,4 +85,4 @@ public class Main {
 		submenu.setMnemonic(KeyEvent.VK_S);
 
 		menuItem = new JMenuItem("An item in the submenu");
-		menuItem.setAccelerator(K
+		menuItem.setAccelerator(KeyStroke.getKeyS
