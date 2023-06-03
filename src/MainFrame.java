f93dbf9acab7cbcc208a39607fd4941109fce0ab
MakerSuite <133332883+MakerSuite@users.noreply.github.com>
2023-06-03 22:58:59 +0000

    Further refactoring.

---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
